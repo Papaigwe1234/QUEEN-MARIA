1ecd9d0440ff19b593c735c78f1947d5437c57aa
PAPA IGWE <papaigwe0@gmail.com>
2024-10-22 14:21:12 +0100

    Update main-donate.js

--- plugins/main-donate.js
+++ plugins/main-donate.js
@@ -3,8 +3,8 @@ let handler = async(m, { conn, usedPrefix, command }) => {
 
     let don = `
 ≡ *DONATION*
-Share 𝚀𝚄𝙴𝙴𝙽 𝙰𝙽𝙽𝙰 Bot Link And Support. Git Link https://github.com/toge012345/QUEEN-ANNA`
-let img = 'https://raw.githubusercontent.com/toge012345/QUEEN-ANNA/main/ANNA.jpg?token=GHSAT0AAAAAACWTLQTA3HTSTXW2SAI4LQGCZXG4LCA'
+Share 𝚀𝚄𝙴𝙴𝙽 MARIA Bot Link And Support. Git Link https://github.com/Papaigwe1234/QUEEN-MARIA`
+let img = 'https://raw.githubusercontent.com/Papaigwe1234/QUEEN-MARIA/main/MARIA.jpg?token=GHSAT0AAAAAACWTLQTA3HTSTXW2SAI4LQGCZXG4LCA'
 conn.sendFile(m.chat, img, 'img.jpg', don, m)
 }
 
