668a4080a649d83cf6562c54c568fe5d7b2a9023
PAPA IGWE <papaigwe0@gmail.com>
2024-10-22 13:03:57 +0100

    Update channel.js

--- plugins/channel.js
+++ plugins/channel.js
@@ -1,10 +1,10 @@
 let handler = async m => m.reply(`
 
-╭⭑⭑⭑★✪ 𝚀𝚄𝙴𝙴𝙽 𝙰𝙽𝙽𝙰 ✪★⭑⭑⭑
-│ 📂 *BOT Name:* 𝚀𝚄𝙴𝙴𝙽 𝙰𝙽𝙽𝙰
-│ 📝 *Description:* _I'm 𝚀𝚄𝙴𝙴𝙽 𝙰𝙽𝙽𝙰. A MultiDevice WhatsApp bot with rich features Created By 𝚃𝙾𝙶𝙴 𝙸𝙽𝚄𝙼𝙰𝙺𝙸_
-│ 👤 *Owner:* 𝚃𝙾𝙶𝙴 𝙸𝙽𝚄𝙼𝙰𝙺𝙸
-│ 🌐 *Channel:* https://whatsapp.com/channel/0029VaiuD4s4IBhI0fzbv40Z
+╭⭑⭑⭑★✪ 𝚀𝚄𝙴𝙴𝙽 MARIA ✪★⭑⭑⭑
+│ 📂 *BOT Name:* QUEEN MARIA
+│ 📝 *Description:* _I'm 𝚀𝚄𝙴𝙴𝙽 MARIA. A MultiDevice WhatsApp bot with rich features Created By DADDY IGWE_
+│ 👤 *Owner:* PAPA IGWE
+│ 🌐 *Channel:* https://whatsapp.com/channel/0029VaiuD4s4IBhI0fzbv
 ╰━━━━━━━━━━━━━━━━╯
 `.trim())
 handler.help = ['channel']
