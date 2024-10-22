90a579323b93d79095faac9b49c8ecf3c89fc339
PAPA IGWE <papaigwe0@gmail.com>
2024-10-22 08:44:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import cfonts from 'cfonts';
 
 const { say } = cfonts;
 
-say("ANNA - MD", {
+say("MARIA - MD", {
   'font': "block",
   'align': "center",
   'colors': ['#ff9900'],
@@ -21,7 +21,7 @@ say("ANNA - MD", {
   'maxLength': '15'
 });
 
-say("QUEEN-ANNA By toge012345._.", {
+say("QUEEN-MARIA par Papaigwe1234._.", {
   'font': "chrome",
   'align': "center",
   'colors': ["red", "magenta"],
@@ -81,7 +81,7 @@ async function start(scriptName) {
     if (exitCode === 0) return;
     fs.watchFile(scriptArgs[0], () => {
       fs.unwatchFile(scriptArgs[0]);
-      start("anna-core.js");
+      start("maria-core.js");
     });
   });
 
@@ -89,7 +89,7 @@ async function start(scriptName) {
     console.error(chalk.red(`Error: ${err}`));
     childProcess.kill();
     isRunning = false;
-    start('anna-core.js');
+    start('maria-core.js');
   });
 
   const pluginsDir = path.join(path.dirname(currentScriptPath), "plugins");
@@ -110,17 +110,17 @@ async function start(scriptName) {
   });
 }
 
-start('anna-core.js');
+start('maria-core.js');
 
 
 process.on("unhandledRejection", () => {
   console.error(chalk.red("Unhandled promise rejection. Bot will restart..."));
-  start("anna-core.js");
+  start("maria-core.js");
 });
 
 
 process.on("exit", exitCode => {
   console.error(chalk.red(`Exited with code: ${exitCode}`));
   console.error(chalk.red("Bot will restart..."));
-  start("anna-core.js");
+  start("maria-core.js");
 });
